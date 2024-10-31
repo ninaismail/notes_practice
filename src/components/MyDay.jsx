4a353addbfc0c731b4a446ac2549c18c49459a3d
ninaismail <najatt.ismail@gmail.com>
2024-10-31 11:19:03 +0200

    designed the notes cards

--- src/components/MyDay.jsx
+++ src/components/MyDay.jsx
@@ -27,33 +27,37 @@ const MyDay = () => {
             {isLoggedIn === true ?
             <>
             <h1 className="lg:text-4xlmd:text-3xl sm:text-2xl text-xl font-bold tracking-wider text-gray-900 mb-6">My Notes for the day!</h1>
-            <div className="grid grid-cols-4 gap-4">
+            <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
                 <button
-                    className="lg:col-span-1 sm:col-span-2 col-span-4 aspect-square p-4 bg-blue-100 rounded-lg shadow"
-                    onClick={() => setOpen(true)} // Corrected onClick placement
+                    className="p-6 bg-blue-100 rounded-2xl shadow min-h-[264px]"
+                    onClick={() => setOpen(true)}
                 >
-                    <h5 className="2xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl text-lg font-bold tracking-tight text-gray-900 rounded-full p-4 bg-white backdrop-blur-lg">
-                        +
-                    </h5>
+                    <h1 className="grid place-content-center 2xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl text-lg font-bold tracking-tight text-gray-900 rounded-full bg-white transform duration-300 hover:scale-105 px-4 py-3">
+                        <span>+</span>
+                    </h1>
                 </button>
 
                 {Array.isArray(notes) && notes.map((item, i) => (
-                    <div key={i}
-                        className="lg:col-span-1 sm:col-span-2 col-span-4 aspect-square p-4 bgblue-100nded-lg shadow">
-                        <h5 className="text-2xl font-bold tracking-tight text-gray-900 rounded-full">
-                            {item?.title}
-                        </h5>
-                        <div className="relative">
-                            <p className="font-normal max-h-10 text-gray-800">
-                                {item?.content}
-                            </p>
-                            <div className="backdrop-blur-lg absolute bottom-10 left-0" />
-                            <a href={`${item?.id}`} className="mx-auto px-4 py-2 text-sm font-bold text-white bg-blue-500 rounded hover:bg-blue-700">
-                                Read More
-                            </a>
-                        </div>
-                    </div>
+                 <div
+                    key={i} 
+                    className="relative w-full p-6 space-y-3 rounded-2xl shadow min-h-[264px]"
+                >
+                    <h1 className="text-2xl font-bold tracking-tight text-gray-900">
+                    {item?.title}
+                    </h1>
+                    <p className="font-normal text-gray-800 line-clamp-4">
+                        {item?.content}
+                    </p>
+                    <hr/> 
+                    <a role="button" href={`${item?.id}`} className="absolute px-4 py-3 flex justify-center items-center w-fit bottom-6 font-bold text-white bg-blue-500 rounded-lg hover:bg-blue-700">
+                        Learn More
+                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
+                </div>
                 ))}
+
             </div>
             {open === true ? (
                 <AddNoteForm onClose={() => setOpen(false)} />
